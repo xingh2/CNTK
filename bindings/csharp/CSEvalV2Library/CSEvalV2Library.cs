7162dec2d0091740c944886ea120ab78913f2373
Zhou Wang <zhouwang@microsoft.com>
2016-11-29 14:21:54 +0100

    add CSEvalV2Library as wrapper

--- bindings/csharp/CSEvalV2Library/CSEvalV2Library.cs
+++ bindings/csharp/CSEvalV2Library/CSEvalV2Library.cs
@@ -7,6 +7,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,38 @@ namespace CNTK.CSharp
 {
     public class Evaluation
     {
+        public Evaluation()
+        {
+            model = null;
+        }
+
+        public void LoadModel(string modelFile, DeviceDescriptor computeDevice)
+        {
+            if (!File.Exists(modelFile))
+            {
+                throw new FileNotFoundException(string.Format("File '{0}' not found.", modelFile));
+            }
+            Function.LoadModel(modelFile, computeDevice);
+        }
+
+        // Todo: set default parameters  = DeviceDescriptor.UseDefaultDevice()
+        public void Evaluate(Dictionary<Variable, Value> arguments, Dictionary<Variable, Value> outputs, DeviceDescriptor computeDevice)
+        {
+            if (model == null)
+            {
+                throw new NullReferenceException("No model is loaded. Please load the model first before evaluation.");
+            }
+
+            // Evaluate the model.
+
+        }
+
+        public void Clone()
+        {
+
+        }
+
+        private Function model;
 
     }
 }
